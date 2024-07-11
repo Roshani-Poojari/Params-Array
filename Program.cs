a1a98e91c621ae7acd4db5d4eb5cbfda7c8f32c6
Roshani-Poojari <135858937+Roshani-Poojari@users.noreply.github.com>
2024-07-11 21:47:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArray
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            SummationOfElements(21,56,64,47,19,83);
+        }
+
+        static void SummationOfElements(params int[] elements){
+            int sum = 0;
+            foreach (int item in elements)
+            {
+                sum += item;
+            }
+            Console.WriteLine("Sum of all elements in params array is: "+sum);
+        }
+
+    }
+}
